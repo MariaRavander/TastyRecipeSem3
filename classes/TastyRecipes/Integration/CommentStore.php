ff7f5047a40af49d5000658c659558f5e901b82e
MariaRavander <maria@ravander.nu>
2017-12-03 15:27:28 +0100

    Add files via upload

---
+++ classes/TastyRecipes/Integration/CommentStore.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace TastyRecipes\Integration;
+
+/**
+ * Handles the files with comments
+ */
+class CommentStore {
+    
+    private $servername = "localhost";
+    private $dbUsername = "root";
+    private $dbPassword = "root";
+    private $databaseName = "myDB";
+    
+    private $page;
+    private $conn;
+
+    public function __construct($page) {
+        $this->page = $page;
+        
+        $this->conn = mysqli_connect($this->servername, $this->dbUsername, $this->dbPassword, $this->databaseName);
+        
+        if(!$this->conn) {
+            die("connetion failed to database: " . mysqli_connect_error);
+        }
+    }
+
+    /**
+     * @return an array of commententries with user, comment and timestamp.
+     */
+    public function getAllComments() {
+        
+        $commentEntries = array();
+
+         $sql = "SELECT *
+                FROM RecipeComments
+                WHERE page = '$this->page'
+                ORDER BY timestamp";  
+        
+        $result = $this->conn->query($sql);
+        
+        $rows = $result->num_rows;
+        
+        for($i=1; $i<=$rows; $i++) {
+            $row = $result->fetch_assoc();
+            array_push($commentEntries, $row["username"]);
+            array_push($commentEntries, $row["comment"]);
+            array_push($commentEntries, $row["timestamp"]);
+            
+        }
+
+        return $commentEntries;
+        
+    }
+
+    public function addComment($user, $comment) {
+
+        $timestamp = date("Y-m-d-h-i-s-a", time());
+        $sql = "INSERT INTO RecipeComments(page, comment, username, timestamp) VALUES ('$this->page', '$comment', '$user', '$timestamp')";
+        
+        mysqli_query($this->conn, $sql);
+
+    }
+    public function deletComment($user, $timestamp) {
+ 
+        $sql = "DELETE FROM RecipeComments
+                WHERE username = '$user' AND timestamp = '$timestamp' AND page = '$this->page'";
+        
+        mysqli_query($this->conn, $sql);
+          
+    }
+}
+?>
+
+
